4b2a15480dc3e3c09e0abda1d74692aea3e0a253
pukekohetech <33167310+pukekohetech@users.noreply.github.com>
2025-11-12 19:49:42 +1300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+/* simple service worker to cache app shell */
+const CACHE_VERSION = 'phs-stamper-v1.0.0';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './service-worker.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_VERSION) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_VERSION).then((cache) => {
+          cache.put(req, clone);
+        });
+        return res;
+      }).catch(() => caches.match('./'));
+    })
+  );
+});
